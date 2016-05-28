9d70a240b9e92be2179b36de2196e1cfdd8f2b98
Domn Werner <domn.werner@gmail.com>
2016-05-27 20:31:20 -0700

    Created project.

---
+++ ShellTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShellTest
+{
+    /// <summary>
+    /// A console application that launches for testing SharpShell.
+    /// </summary>
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
+ 
